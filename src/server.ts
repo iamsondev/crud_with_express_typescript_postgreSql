24bbeca98489f391a745b97a248363f4ae5abac1
iamsondev <sondipkumarsk@gmail.com>
2025-12-02 22:59:02 +0600

    initial project setup

---
+++ src/server.ts
@@ -0,0 +1,57 @@
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+const app = express();
+
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTING_STR}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    age INT,
+    phone VARCHAR(15),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )      
+    `);
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos(
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      title VARCHAR(200) NOT NULL,
+      description TEXT,
+      completed BOOLEAN DEFAULT false,
+      due_date DATE,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )`);
+};
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World, i am learning express.js on details !");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    status: true,
+    message: "API is working",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
